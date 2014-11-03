2cd9c16fd23d06033a5b4ca9be4d74e0aa7e40f4
fizalihsan <fizalihsan@gmail.com>
2014-11-02 20:16:12 -0500

    Fixing file name

---
+++ src/main/groovy/util/EbookParser.groovy
@@ -0,0 +1,47 @@
+package util
+
+import java.util.concurrent.Callable
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+/**
+ * This is a fun program to print book titles from http://it-ebooks.info/book
+ */
+
+def range = 4245..9000
+def pool = Executors.newFixedThreadPool(10)
+def latch = new CountDownLatch(range.size())
+range.each { id ->
+    bookDetailPrinter = {
+        def url = "http://it-ebooks.info/book/$id"
+//        println "At $url..."
+        try {
+            def text = url.toURL().getText(connectTimeout: 120_000, readTimeout: 120_000, useCaches: true)
+            def title = getTextWithin(text, "<title>", "</title>")
+            def year = getTextWithin(text, "datePublished\">", "</b>")
+
+            if(year.length()>4 && year.contains("<")){
+                println "$url - !!! Book not found !!!"
+            } else {
+                println "$url - $title - $year"
+            }
+
+
+        } catch (e) {
+            println "$url - Failed - ${e.message}"
+        } finally{
+            latch.countDown()
+        }
+    }
+    pool.submit(bookDetailPrinter as Callable)
+}
+latch.await()
+println "************************ Shutting down ****************************"
+pool.shutdownNow()
+
+
+def getTextWithin(String text, String startTag, String endTag) {
+    def from = text.indexOf(startTag) + startTag.length()
+    def to = text.indexOf(endTag, from)
+    text.substring(from, to)
+}
